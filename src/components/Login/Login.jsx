ba64aaafc7ca2b67fcf6f06192573c2c025de8ed
devsamin <abdullahalsamin79@gmail.com>
2025-08-01 06:51:20 -0700

    Display User Info And sign out

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,24 +1,50 @@
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import React from 'react';
+import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import React, { useState } from 'react';
 import auth from '../../Firebase/Firebase_init';
 
 const Login = () => {
 
-    const provider = new GoogleAuthProvider()
+    const provider = new GoogleAuthProvider();
+    const [user, setUser] = useState(null);
 
-    const hendalGooglesignIn = () =>{
-        console.log(auth, provider)
-        signInWithPopup(auth,provider)
-        .then((res)=>{
-            console.log(res)
+    const hendalGooglesignIn = () => {
+        signInWithPopup(auth, provider)
+            .then((res) => {
+                console.log(res.user); // ঠিক প্রপার্টি
+                setUser(res.user);     // ঠিকভাবে সেট করা
+            })
+            .catch(error => {
+                console.log("ERROR", error);
+                setUser(null);
+            });
+    };
+    const hendalSignout=()=>{
+        signOut(auth)
+        .then(()=>{
+            console.log("Sign out successfully ")
+            setUser(null)
         })
-        .catch(error=>{
-            console.log("ERROR" ,error)
+        .catch(er => {
+            console.log(er)
+            
         })
     }
+
     return (
         <div>
-            <button onClick={hendalGooglesignIn}>Hello Google</button>
+            {/* <button onClick={hendalGooglesignIn}>Sign in with Google</button>
+            <button onClick={hendalSignout}>Sign out</button> */}
+            {
+                user ? <button onClick={hendalSignout}>Sign out</button> : <button onClick={hendalGooglesignIn}>Sign in with Google</button>
+            }
+            
+                {
+                    user && <div>
+                        <h2>{user.displayName}</h2>
+                        <h2>{user.email}</h2>
+                    </div>
+                }
+            
         </div>
     );
 };
